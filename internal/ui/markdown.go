27ad876bc2f3107bb801b7d7e16f10f63c57af26
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-01-19 20:45:52 -0300

    fix: improve markdown report
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- internal/ui/markdown.go
+++ internal/ui/markdown.go
@@ -17,14 +17,21 @@ func WriteProjectMarkdown(db *badger.DB, project string, w io.Writer) error {
 		return err
 	}
 
+	if len(tasks) == 0 {
+		return fmt.Errorf("project %s has no tasks", project)
+	}
+
 	_, _ = fmt.Fprintln(w, "# "+project+"\n")
-	_, _ = fmt.Fprintf(w, "> Total time: %s\n\n", sumTasksTimes(tasks).Round(time.Second).String())
+	_, _ = fmt.Fprintf(w, "> Total time: %s\n", sumTasksTimes(tasks).Round(time.Second).String())
+	_, _ = fmt.Fprintf(w, "> From: %s\n", tasks[len(tasks)-1].StartAt.Format(time.UnixDate))
+	_, _ = fmt.Fprintf(w, "> To: %s\n\n", tasks[0].EndAt.Format(time.UnixDate))
+
 	for _, task := range tasks {
 		_, _ = fmt.Fprintf(
 			w,
-			"- %s (#%d) - %s\n",
-			task.Title,
+			"- [x] #%d %s - %s\n",
 			task.ID+1,
+			task.Title,
 			task.EndAt.Sub(task.StartAt),
 		)
 	}
