9272bbfd209e32f135f57a54e90aa8243e55c0ef
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-01-18 23:44:48 -0300

    feat: completions
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- go.mod
+++ go.mod
@@ -8,5 +8,5 @@ require (
 	github.com/dgraph-io/badger/v3 v3.0.0-20210114060103-a35b08db404b
 	github.com/muesli/reflow v0.2.1-0.20210115123740-9e1d0d53df68
 	github.com/muesli/termenv v0.7.4
-	github.com/spf13/cobra v0.0.5
+	github.com/spf13/cobra v1.1.1
 )
--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	rootCmd.PersistentFlags().StringVarP(&project, "project", "p", "default", "Project name")
 	reportCmd.PersistentFlags().StringVarP(&output, "output", "o", "", "Where to save the markdown report file (default \"{project}.md\")")
 
-	rootCmd.AddCommand(reportCmd)
+	rootCmd.AddCommand(reportCmd, completionsCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -30,8 +30,8 @@ func main() {
 }
 
 var rootCmd = &cobra.Command{
-	Use:   "tasktimer",
-	Short: "Task Timer is a dead simple TUI task timer",
+	Use:   "tt",
+	Short: "Task Timer (tt) is a dead simple TUI task timer",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		db, err := setup()
 		if err != nil {
@@ -113,4 +113,45 @@ func (b badgerStdLoggerAdapter) Infof(s string, i ...interface{}) {
 
 func (b badgerStdLoggerAdapter) Debugf(s string, i ...interface{}) {
 	log.Printf("[DEBUG] "+s, i...)
+}
+
+var completionsCmd = &cobra.Command{
+	Use:   "completion [bash|zsh|fish]",
+	Short: "Print shell autocompletion scripts for tt",
+	Long: `To load completions:
+Bash:
+$ source <(goreleaser completion bash)
+# To load completions for each session, execute once:
+Linux:
+  $ tt completion bash > /etc/bash_completion.d/tt
+MacOS:
+  $ tt completion bash > /usr/local/etc/bash_completion.d/tt
+Zsh:
+  # If shell completion is not already enabled in your environment you will need
+  # to enable it.  You can execute the following once:
+  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
+  # To load completions for each session, execute once:
+  $ tt completion zsh > "${fpath[1]}/_tt"
+  # You will need to start a new shell for this setup to take effect.
+Fish:
+  $ tt completion fish | source
+  # To load completions for each session, execute once:
+  $ tt completion fish > ~/.config/fish/completions/tt.fish
+`,
+	SilenceUsage:          true,
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh", "fish"},
+	Args:                  cobra.ExactValidArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var err error
+		switch args[0] {
+		case "bash":
+			err = cmd.Root().GenBashCompletion(cmd.OutOrStdout())
+		case "zsh":
+			err = cmd.Root().GenZshCompletion(cmd.OutOrStdout())
+		case "fish":
+			err = cmd.Root().GenFishCompletion(cmd.OutOrStdout(), true)
+		}
+		return err
+	},
 }
