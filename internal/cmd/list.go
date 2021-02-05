195c49411aabf22769eb93e21ae5e81e0999bea6
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-02-05 00:05:31 -0300

    feat: list projects
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

---
+++ internal/cmd/list.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/charmbracelet/glamour"
+	"github.com/mattn/go-isatty"
+	gap "github.com/muesli/go-app-paths"
+	"github.com/spf13/cobra"
+)
+
+type listCmd struct {
+	cmd *cobra.Command
+}
+
+func newListCmd() *listCmd {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "List all projects",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			home := gap.NewScope(gap.User, "tasktimer")
+			datas, err := home.DataDirs()
+			if err != nil {
+				return err
+			}
+
+			var buf bytes.Buffer
+			for _, data := range datas {
+				if _, err := os.Stat(data); err != nil && os.IsNotExist(err) {
+					continue
+				}
+				if err := filepath.Walk(data, func(path string, info os.FileInfo, err error) error {
+					if err != nil {
+						return err
+					}
+					if filepath.Ext(path) == ".db" {
+						_, _ = fmt.Fprintln(&buf, "- "+strings.Replace(filepath.Base(path), ".db", "", 1))
+						return filepath.SkipDir
+					}
+					return nil
+				}); err != nil {
+					return err
+				}
+			}
+
+			if isatty.IsTerminal(os.Stdout.Fd()) {
+				rendered, err := glamour.RenderWithEnvironmentConfig(buf.String())
+				if err != nil {
+					return err
+				}
+				fmt.Print(rendered)
+				return nil
+			}
+
+			fmt.Print(buf.String())
+			return nil
+		},
+	}
+
+	return &listCmd{cmd: cmd}
+}
--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -49,7 +49,14 @@ func newRootCmd(version string, exit func(int)) *rootCmd {
 
 	cmd.PersistentFlags().StringVarP(&root.project, "project", "p", "default", "Project name")
 
-	cmd.AddCommand(newRerportCmd().cmd, newCompletionCmd().cmd, newPathsCmd().cmd, newToJSONCmd().cmd, newFromJSONCmd().cmd)
+	cmd.AddCommand(
+		newRerportCmd().cmd,
+		newCompletionCmd().cmd,
+		newPathsCmd().cmd,
+		newToJSONCmd().cmd,
+		newFromJSONCmd().cmd,
+		newListCmd().cmd,
+	)
 
 	root.cmd = cmd
 	return root
