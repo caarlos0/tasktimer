ba175986674404a4845f1d9886692b71ae524943
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-04-12 10:21:36 -0300

    fix: import
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- internal/ui/common.go
+++ internal/ui/common.go
@@ -1,8 +1,6 @@
 package ui
 
-import (
-	"github.com/charmbracelet/lipgloss"
-)
+import "github.com/charmbracelet/lipgloss"
 
 // light palette: https://colorhunt.co/palette/201882
 // dark palette:  https://colorhunt.co/palette/273948
