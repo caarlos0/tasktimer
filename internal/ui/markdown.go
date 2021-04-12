1d4ab9d9d4f85c1e978b6cb5336e4924ec9971ec
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-04-12 10:28:40 -0300

    fix: quote
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- internal/ui/markdown.go
+++ internal/ui/markdown.go
@@ -24,7 +24,7 @@ func WriteProjectMarkdown(db *badger.DB, project string, w io.Writer) error {
 	_, _ = fmt.Fprintln(w, "# "+project+"\n")
 	_, _ = fmt.Fprintf(
 		w,
-		"❯ Total time **%s**, timed between **%s** and **%s**\n\n",
+		"> Total time **%s**, timed between **%s** and **%s**\n\n",
 		sumTasksTimes(tasks).Round(time.Second).String(),
 		tasks[len(tasks)-1].StartAt.Format("2006-01-02"),
 		tasks[0].EndAt.Format("2006-01-02"),
