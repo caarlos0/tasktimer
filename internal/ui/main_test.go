fdcb3e87b1b12ae944795333937cd917fac5c8cc
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-06-21 10:37:38 -0300

    test
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- internal/ui/main_test.go
+++ internal/ui/main_test.go
@@ -2,16 +2,19 @@ package ui
 
 import (
 	"io"
+	"log"
 	"path/filepath"
 	"testing"
 	"time"
 
+	"github.com/caarlos0/tasktimer/internal/store"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/bubbletea/teatest"
 	"github.com/dgraph-io/badger/v3"
 )
 
 func TestApp(t *testing.T) {
+	log.SetOutput(io.Discard)
 	options := badger.DefaultOptions(filepath.Join(t.TempDir(), "db")).
 		WithLoggingLevel(badger.ERROR)
 	db, err := badger.Open(options)
@@ -31,4 +34,18 @@ func TestApp(t *testing.T) {
 	}, func(out []byte) {
 		teatest.RequireEqualOutput(t, out)
 	})
+
+	tasks, err := store.GetTaskList(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(tasks) != 1 {
+		t.Fatalf("expected 1 task, got %d", len(tasks))
+	}
+
+	task := tasks[0]
+	if d := task.EndAt.Sub(task.StartAt); d != time.Second {
+		t.Fatalf("expected 1 second task, got %v", d)
+	}
 }
